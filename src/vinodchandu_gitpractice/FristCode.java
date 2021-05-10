db4b419cb254f8af104b36921564fe8958f0d172
chandu velagapudi <chandu.velagapudi22@gmail.com>
2021-05-10 13:00:08 +0530

    after cloned the code i added a new file  name modi to print the logics

--- src/vinodchandu_gitpractice/FristCode.java
+++ src/vinodchandu_gitpractice/FristCode.java
@@ -10,10 +10,16 @@ public class FristCode {
 		System.out.println("THIS IS m2() instance method avillable in FristCode class");
 	}
 
+	public void modi() {
+		System.out.println("i created a new method modi method after clone my code");
+		System.out.println("THIS METHOD I ADDED AFTER I CLONED MY PROJECT TO NEW BRACH");
+	}
+
 	public static void main(String[] args) {
 		FristCode code = new FristCode();
 		m1();
 		code.m2();
+		code.modi();
 
 	}
 }
